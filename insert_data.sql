6068d595a57bc47e67044c436dedd53e01cb1b05
Shashwat38 <shatiw0@gmail.com>
2026-01-07 03:18:17 +0100

    insert_data.sql

--- insert_data.sql
+++ insert_data.sql
@@ -23,16 +23,16 @@ INSERT INTO department (department_name, manager_id) VALUES
 ('Information Technology', 3);
 
 INSERT INTO employee (employment_id, skill_set, salary, manager, person_id, department_id, job_title_id) VALUES
-('EMP001', 'Databases, SQL', '45000', 'Manager A', 1, 1, 1),
-('EMP002', 'Mathematics, Teaching', '40000', 'Manager B', 2, 2, 2),
-('EMP003', 'Programming, Algorithms', '42000', 'Manager C', 3, 1, 3),
-('EMP004', 'Machine Learning', '38000', 'Manager A', 4, 1, 4),
-('EMP005', 'Teaching, Grading', '30000', 'Manager B', 5, 2, 5),
-('EMP006', 'Math, Algebra', '39000', 'Manager B', 6, 2, 2),
-('EMP007', 'Data Analysis', '37000', 'Manager C', 7, 3, 3),
-('EMP008', 'AI Research', '41000', 'Manager A', 8, 1, 1),
-('EMP009', 'Networks', '43000', 'Manager C', 9, 3, 4),
-('EMP010', 'Discrete Math', '35000', 'Manager B', 10, 2, 5);
+('EMP001', 'Databases, SQL', 45000, 'Manager A', 1, 1, 1),
+('EMP002', 'Mathematics, Teaching', 40000, 'Manager B', 2, 2, 2),
+('EMP003', 'Programming, Algorithms', 42000, 'Manager C', 3, 1, 3),
+('EMP004', 'Machine Learning', 38000, 'Manager A', 4, 1, 4),
+('EMP005', 'Teaching, Grading', 30000, 'Manager B', 5, 2, 5),
+('EMP006', 'Math, Algebra', 39000, 'Manager B', 6, 2, 2),
+('EMP007', 'Data Analysis', 37000, 'Manager C', 7, 3, 3),
+('EMP008', 'AI Research', 41000, 'Manager A', 8, 1, 1),
+('EMP009', 'Networks', 43000, 'Manager C', 9, 3, 4),
+('EMP010', 'Discrete Math', 35000, 'Manager B', 10, 2, 5);
 
 INSERT INTO skill (skill_name) VALUES
 ('SQL'),
@@ -60,7 +60,10 @@ INSERT INTO teaching_activity (activity_name, factor) VALUES
 ('Lab', 2.4),
 ('Tutorial', 2.4),
 ('Seminar', 1.8),
-('Other', 1.0);
+('Other', 1.0),
+('Admin', 1.0),
+('Exam', 1.0);
+
 
 INSERT INTO planned_activity (course_instance_id, teaching_activity_id, planned_hours) VALUES
 (2,1,20),
@@ -72,7 +75,12 @@ INSERT INTO planned_activity (course_instance_id, teaching_activity_id, planned_
 (4,2,35),
 (4,3,20),
 (4,4,15),
-(4,5,10);
+(4,5,10),
+(2,6,83),
+(2,7,177),
+(4,6,73),
+(4,7,140.75);
+
 
 INSERT INTO allocation (course_instance_id, teaching_activity_id, employee_id, allocated_hours) VALUES
 (2,1,1,10),
@@ -85,7 +93,20 @@ INSERT INTO allocation (course_instance_id, teaching_activity_id, employee_id, a
 (4,2,7,15),
 (4,3,8,10),
 (4,4,9,10),
-(4,5,10,8);
+(4,5,10,8),
+(2,6,6,45),
+(2,6,7,38),
+(2,7,6,90),
+(2,7,8,87),
+(4,6,1,40),
+(4,6,2,33),
+(4,7,1,70.75),
+(4,7,3,70);
+
+
+
+
+
 
 
 
